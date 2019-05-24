22a405743139afa98dbf6e815453a80866616fb0
dnkantor <dnkantor@gmail.com>
2019-05-23 20:28:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const twilio = require('twilio');
 
-const accountSid = 'ACbc98dc6e6611d07a49fecf14dba35958';
-const authToken = '219dc6db55938d524334b2deb2a9e225';
+const accountSid = '********************************';
+const authToken = '********************************';
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
-     body: 'Hi Papa',
-     from: '+12044003933',
-     to: '+19782964802'
+     body: 'Hi!',
+     from: '+1***********',
+     to: '+1***********'
    })
   .then(message => console.log(message))
   .catch(err => {
